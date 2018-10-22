b07f08239ae8c2ef393b1d027ef7609fe41c4818
Nicolas Huynh <nico@macbook-pro-de-nicolas.home>
2018-10-22 03:06:39 +0200

    cleanup

--- docs/test.sh
+++
@@ -1,7 +0,0 @@
-# Desktop shotcuts for switching spaces (effective after closing session)
-defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 118 "<dict><key>enabled</key><true/><key>value</key><dict><key>type</key><string>standard</string>\
-<key>parameters</key><array><integer>65535</integer><integer>18</integer><integer>262144</integer></array></dict></dict>"
-defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 119 "<dict><key>enabled</key><true/><key>value</key><dict><key>type</key><string>standard</string>\
-<key>parameters</key><array><integer>65535</integer><integer>19</integer><integer>262144</integer></array></dict></dict>"
-defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 120 "<dict><key>enabled</key><true/><key>value</key><dict><key>type</key><string>standard</string>\
-<key>parameters</key><array><integer>65535</integer><integer>20</integer><integer>262144</integer></array></dict></dict>"
